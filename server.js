fa878690a447964e5e0ba3cf0a1fa1524bf06d0f
DN050498MAM <andrii.moskalenko@privatbank.ua>
2022-12-10 16:47:42 +0200

    Add lesson4

--- server.js
+++ server.js
@@ -5,17 +5,50 @@ const db = require('./database');
 
 const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
 const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
+const authFile = fs.readFileSync(path.join(__dirname, 'static', 'auth.js'));
 const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+const registerFile = fs.readFileSync(path.join(__dirname, 'static', 'register.html'));
 
 const server = http.createServer((req, res) => {
+  if(req.method === 'GET') {
     switch(req.url) {
-        case '/': return res.end(indexHtmlFile);
-        case '/script.js': return res.end(scriptFile);
-        case '/style.css': return res.end(styleFile);
+      case '/': return res.end(indexHtmlFile);
+      case '/script.js': return res.end(scriptFile);
+      case '/auth.js': return res.end(authFile);
+      case '/style.css': return res.end(styleFile);
+      case '/register': return res.end(registerFile);
     }
-    return res.end('Error 404');
+  }
+  if(req.method === 'POST') {
+    switch(req.url) {
+      case '/api/register': return registerUser(req, res);
+    }
+  }
+  return res.end('Error 404');
 });
 
+function registerUser(req, res) {
+    let data = '';
+    req.on('data', function(chunk) {
+        data += chunk;
+    });
+    req.on('end', async function() {
+      try {
+        const user = JSON.parse(data);
+        if(!user.login || !user.password) {
+          return res.end('Empty login or password');
+        }
+        if(await db.isUserExist(user.login)) {
+          return res.end('User already exist');
+        }
+        await db.addUser(user);
+        return res.end('Registeration is successfull');
+      }
+      catch(e) {
+        return res.end('Error: ' + e);
+      }
+    });
+}
 
 server.listen(3000);
 
